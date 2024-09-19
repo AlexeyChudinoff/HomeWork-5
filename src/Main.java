05ab5a00aaf07db4a5643e271b8afecb8d306c15
AlexeyChudinoff <201900@mail.ru>
2024-09-20 00:37:05 +0700

    HomeWork 5.1-4

--- src/Main.java
+++ src/Main.java
@@ -58,9 +58,48 @@ public class Main {
       System.out.println("извините, доставка не осуществляется");
     }
 
-
-
-
+    System.out.println(" Задача5");
+    int monthNumber = 15;
+    switch (monthNumber) {
+      case 1:
+        System.out.println("Зима");
+        break;
+      case 2:
+        System.out.println("Зима");
+        break;
+      case 3:
+        System.out.println("Весна");
+        break;
+      case 4:
+        System.out.println("Весна");
+        break;
+      case 5:
+        System.out.println("Весна");
+        break;
+      case 6:
+        System.out.println("Лето");
+        break;
+      case 7:
+        System.out.println("Лето");
+        break;
+      case 8:
+        System.out.println("Лето");
+        break;
+      case 9:
+        System.out.println("Осень");
+        break;
+      case 10:
+        System.out.println("Осень");
+        break;
+      case 11:
+        System.out.println("Осень");
+        break;
+      case 12:
+        System.out.println("Зима");
+        break;
+      default:
+        System.out.println("Нет такого месяца в году");
+    }
 
 
   }
