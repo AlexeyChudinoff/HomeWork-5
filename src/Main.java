fe462bc83e640b39bb7e0a8d6fb6169e21f47a35
AlexeyChudinoff <201900@mail.ru>
2024-09-19 19:49:39 +0700

    HomeWork 5.1-3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,48 @@
 public class Main {
 
   public static void main(String[] args) {
-    //System.out.println("Hello world!");
 
-
-
+    System.out.println(" Задача 1");
+    int clientOs = 0;
+    if (clientOs == 0) {
+      System.out.println("Установите версию приложения для iOS по ссылке");
+    } else {
+      System.out.println("Установите версию приложения для Android по ссылке");
+    }
+    System.out.println(" Задача 2");
+    int clientDeviceYear = 2014;
+    switch (clientOs) {
+      case 0:
+        if (clientDeviceYear >= 2015) {
+          System.out.println("Установите версию приложения для IOS по ссылке:");
+        } else {
+          System.out.println("Установите облегченную версию приложения для iOS по ссылке:");
+        }
+        break;
+      case 1:
+        if (clientDeviceYear >= 2015) {
+          System.out.println("Установите версию приложения для Android по ссылке:");
+        } else {
+          System.out.println("Установите облегченную версию приложения для Android по ссылке:");
+        }
+        break;
+      default:
+        System.out.println("Такая OS  не поддерживается ");
+    }
+    System.out.println(" Задача 3");
+    //int ferstVisokosYear =1584;
+    int year = 1993;
+    if (year % 4 != 0) {
+      System.out.println(" не високосный год не делится на 4");
+    } else if (year % 400 == 0) {
+      System.out.println(" високосный год (делится на 400)");
+    } else if (year % 100 == 0) {
+      System.out.println(
+          " не високосный год (делится на 100, но не на 400)");
+    } else {
+      System.out.println(
+          " високосный (не делится на 100, но делится на 4)");
+    }
   }
 }
+
